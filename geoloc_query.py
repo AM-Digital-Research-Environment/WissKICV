bb741eb171d93e49e6d848ce4a94eb41fd41b7c5
Neeraj Viswajith <Neeraj.Thandayan-Viswajith@uni-bayreuth.de>
2024-08-14 15:01:19 +0200

    Create geoloc_query.py

---
+++ geoloc_query.py
@@ -0,0 +1,30 @@
+# pip install sparqlwrapper
+# https://rdflib.github.io/sparqlwrapper/
+
+import sys
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+endpoint_url = "https://query.wikidata.org/sparql"
+
+query = """SELECT ?country ?countryLabel ?state ?stateLabel WHERE {
+  ?country wdt:P31 wd:Q6256 .
+  OPTIONAL {
+  ?country wdt:P150 ?state .
+  }
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+}"""
+
+
+def get_results(endpoint_url, query):
+    user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
+    # TODO adjust user agent; see https://w.wiki/CX6
+    sparql = SPARQLWrapper(endpoint_url, agent=user_agent)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    return sparql.query().convert()
+
+
+results = get_results(endpoint_url, query)
+
+for result in results["results"]["bindings"]:
+    print(result)
